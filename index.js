e3825e3d871e4a4b0b8e604c0aa469b9efa184fe
ciera <cieranicole.m15@gmail.com>
2022-03-16 20:00:43 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {}
+employee.name = 'Nemo'
+employee.address = 'P.Sherman 42 Wallaby Way Sydney'
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee}
+    newEmployee[key] = value
+    return newEmployee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+   const  newEmployee = employee
+   newEmployee[key] = value
+    return newEmployee
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee}
+    delete newEmployee['name']
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    const  newEmployee = employee
+    delete newEmployee['name']
+    return newEmployee
+}
